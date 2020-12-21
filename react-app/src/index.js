11ce6b54fee68914cce2ac83e1a5d49b368d6400
= <zainr7989@gmail.com>
2020-12-20 19:31:20 -0500

    Render 'Hello World' by using React and ReactDOM.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,12 @@
+// import objects from the module (named 'react')
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// define an element using JSX
+const element = <h1>Hello World</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// log the new element
+console.log(element);
+
+// render the React element (defined above using JSX) on the Real DOM
+ReactDOM.render(element, document.getElementById('root'));
