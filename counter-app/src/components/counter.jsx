840df6cbfa46d1fb90629442044954f4dd2f45ad
= <zainr7989@gmail.com>
2020-12-20 20:35:08 -0500

    Add a Counter component in a new react component.

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,11 @@
+ import React, { Component } from 'react';
+
+ // first React Component!
+ class Counter extends Component {
+     // when rendered, this component renders a header
+     render() { 
+         return <h1>Hello World</h1>;
+     }
+ }
+  
+ export default Counter;
