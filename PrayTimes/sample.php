8ba4a0ebb2dd8e729b1dff6bce675c7f09ccf565
= <zainr7989@gmail.com>
2020-12-25 13:43:32 -0500

    Rotate the prayer times table to be vertical.

--- PrayTimes/sample.php
+++ PrayTimes/sample.php
@@ -1,73 +1,97 @@
 <?php
-	
-	// Prayer Times Calculator, Sample Usage
-	// By: Hamid Zarrabi-Zadeh
-	// Edited by Zain Raza in December 2020 - added $month and $day as inputs
-	// Inputs : $method, $year, $latitude, $longitude, $timeZone, $month, $day
-	
-	include('PrayTime.php');
 
-	// default values for the form
-	if (!isset($method) || !isset($year) )
-		list($method, $year, $latitude, $longitude, $timeZone, $month, $day) = (
-			array(0, 2020, 42.2167, -71.5328, -5,
-				 12, 25 
-			)
-		);
-	
+// Prayer Times Calculator, Sample Usage
+// By: Hamid Zarrabi-Zadeh
+// Edited by Zain Raza in December 2020 - added $month and $day as inputs
+// Inputs : $method, $year, $latitude, $longitude, $timeZone, $month, $day
+
+include('PrayTime.php');
+
+// default values for the form
+if (!isset($method) || !isset($year))
+	list($method, $year, $latitude, $longitude, $timeZone, $month, $day) = (array(
+		0, 2020, 42.2167, -71.5328, -5,
+		12, 25
+	));
+
+?>
+
+<?php
+
+$prayTime = new PrayTime($method);
+
+// get the prayer times for just the month, day, and year specified
+$date = strtotime($year . "-$month-$day");
+$times = $prayTime->getPrayerTimes($date, $latitude, $longitude, $timeZone);
+$day = date('M d', $date);
+
+// make an array of the timings
+$prayerNames = [
+	"Fajr", "Sunrise", "Dhuhr", "Asr", "Sunset", "Maghrib", "Isa"
+];
+
+
+// map the name of each time to it's specific timing
+foreach ($times as $index => $time) {
+
+	$prayerName = $prayerNames[$index];
+	$prayerTimes[$prayerName] = "$time";
+
+}
+
 ?>
 <html>
+
 <head>
 	<title>Prayer Timetable</title>
 </head>
 <style>
-	pre {font-family: courier, serif, size: 10pt; margin: 0px 8px;}
+	pre {
+		font-family: courier, serif, size: 10pt;
+		margin: 0px 8px;
+	}
 </style>
 
 <body>
 
-<h1> Prayer Timetable </h1>
-<form name="form" method="post" action="<?php echo $PHP_SELF ?>">
-<div style="padding:10px; background-color: #F8F7F4; border: 1px dashed #EAE9CD;">
+	<h1> Prayer Timetable </h1>
+	<form name="form" method="post" action="<?php echo $PHP_SELF ?>">
+		<div style="padding:10px; background-color: #F8F7F4; border: 1px dashed #EAE9CD;">
 
-	Latitude: <input type="text" value="<?php echo $latitude ?>" name="latitude" size="4">
-	Longitude: <input type="text" value="<?php echo $longitude ?>" name="longitude" size="4">
-	Time Zone: <input type="text" value="<?php echo $timeZone ?>" name="timeZone" size="2"> 
-	Year: <input type="text" value="<?php echo $year ?>" name="year" size="4"> <br>
-	Method: 
-	<select id="method" name="method" size="1" onchange="document.form.submit()">
-		<option value="0">Shia Ithna-Ashari</option>
-    </select>	
-	<input type="submit" value="Make Timetable">
+			Latitude: <input type="text" value="<?php echo $latitude ?>" name="latitude" size="4">
+			Longitude: <input type="text" value="<?php echo $longitude ?>" name="longitude" size="4">
+			Time Zone: <input type="text" value="<?php echo $timeZone ?>" name="timeZone" size="2">
+			Year: <input type="text" value="<?php echo $year ?>" name="year" size="4"> <br>
+			Method:
+			<select id="method" name="method" size="1" onchange="document.form.submit()">
+				<option value="0">Shia Ithna-Ashari</option>
+			</select>
+			<input type="submit" value="Make Timetable">
 
-</div>
-</form>
+		</div>
+	</form>
 
-<pre>
- Date   Fajr   Sunrise  Dhuhr    Asr   Sunset  Maghrib  Isha 
--------------------------------------------------------------
-<?php
+	<!-- Prayer Times in 2 Columns -->
+	<table>
+		<tbody>
+			<?php 
+				// loop through the prayers and their times
+				foreach($prayerTimes as $prayer=>$time) {
+					echo "
+						<tr>
+							<td>{$prayer}: {$time}</td>
+						</tr>
+					";
+				}
+			?>
+		</tbody>
+	</table>
 
-	$prayTime = new PrayTime($method);
-
-	// get the prayer times for just the month, day, and year specified
-	$date = strtotime($year . "-$month-$day");
-	$times = $prayTime->getPrayerTimes($date, $latitude, $longitude, $timeZone);
-	$day = date('M d', $date);
-	print $day . "\t" . implode("\t", $times) . "\n";
-
-?>
-</pre>
-
-<script type="text/javascript">
-	
-	var method = <?php echo $method ?>;
-	document.getElementById('method').selectedIndex = Math.min(method, 6);
-
-</script>
+	<script type="text/javascript">
+		var method = <?php echo $method ?>;
+		document.getElementById('method').selectedIndex = Math.min(method, 6);
+	</script>
 
 </body>
+
 </html>
-
-
-
